d366ffa78c159b567f3ea77151be198cee14e57c
idanbau <81595179+idanbau@users.noreply.github.com>
2022-03-14 01:08:16 +0200

    ex1

--- ex1.py
+++ ex1.py
@@ -1,8 +1,13 @@
 import os
 import re
 
-MATCH_PATTERN = "deep"
+#match pattern to check for files in
+MATCH_PATTERN: str = "deep"
 
 
 def deep_files(path: str):
+    """
+    :param path: the current path we'd like to check
+    :return: the files that accept the rule in the pattern
+    """
     return [f for f in os.listdir(path) if re.search(MATCH_PATTERN, f)]
